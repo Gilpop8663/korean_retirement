351296b5fba32695a039931d7358a4203a284e80
gilpop8663 <wolfye@naver.com>
2023-01-04 03:03:11 +0900

    fix(Index): 카카오톡 웹 환경에서 클립보드 안되는 것 수정 및 드래그 방지 구현

--- pages/index.tsx
+++ pages/index.tsx
@@ -98,7 +98,31 @@ export default function Index() {
         });
       return;
     }
-    alert(SERVICE_MESSAGE.notAbleCopy);
+
+    const tmpTextarea = document.createElement('textarea');
+
+    // 2. 해당 element에 복사하고자 하는 value 저장
+    tmpTextarea.value = window.location.href;
+
+    // 3. 해당 element를 화면에 안보이는 곳에 위치
+    tmpTextarea.setAttribute('readonly', '');
+    tmpTextarea.style.position = 'absolute';
+    tmpTextarea.style.left = '-9999px';
+    document.body.appendChild(tmpTextarea);
+
+    // 4. 해당 element의 value를 시스템 함수를 호출하여 복사
+    tmpTextarea.select();
+    tmpTextarea.setSelectionRange(0, 9999); // 셀렉트 범위 설정
+    const successChk = document.execCommand('copy');
+
+    // 5. 해당 element 삭제
+    document.body.removeChild(tmpTextarea);
+
+    if (!successChk) {
+      alert(SERVICE_MESSAGE.notAbleCopy);
+    } else {
+      alert(SERVICE_MESSAGE.successCopy);
+    }
   };
 
   const onResetClick = () => {
