6b82ecc5b925ede683c94fb9ebeb240ef4d8d768
gilpop8663 <wolfye@naver.com>
2023-01-04 10:27:30 +0900

    chore(GoogleAdsense): 광고 안달리는 문제 테스트

--- pages/index.tsx
+++ pages/index.tsx
@@ -256,6 +256,15 @@ export default function Index() {
       )}
       {!isResult && !result && (
         <>
+          <div className="relative right-0 left-0  z-50 mx-auto h-48  max-w-lg  small:h-36 tall:h-36 long:h-48">
+            <ins
+              className="adsbygoogle"
+              style={{ display: 'block' }}
+              data-ad-client="ca-pub-2859963355226699"
+              data-ad-slot="2860300775"
+              data-ad-format="auto"
+              data-full-width-responsive="true"></ins>
+          </div>
           <Copyright textColor="black" />
         </>
       )}
