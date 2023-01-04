99abaeb947206d7623fb92215079e09281cc5b6f
gilpop8663 <wolfye@naver.com>
2023-01-05 00:22:04 +0900

    fix(GoogleAdsense): 바탕색 안 보이게  수정

--- components/GoogleAdsense.tsx
+++ components/GoogleAdsense.tsx
@@ -2,15 +2,13 @@ import React from 'react';
 
 export default function GoogleAdsense() {
   return (
-    <div className="absolute right-0 left-0 bottom-24 z-50 mx-auto h-48 max-w-lg  bg-slate-200  small:h-36 tall:h-36 long:h-48">
-      <ins
-        className="adsbygoogle"
-        style={{ display: 'block' }}
-        data-ad-client="ca-pub-2859963355226699"
-        data-ad-slot="2860300775"
-        data-ad-format="auto"
-        data-full-width-responsive="true"></ins>
-    </div>
+    <ins
+      className="adsbygoogle absolute right-0 left-0 bottom-24 z-50 mx-auto h-48 max-w-lg small:h-36 tall:h-36 long:h-48"
+      style={{ display: 'block' }}
+      data-ad-client="ca-pub-2859963355226699"
+      data-ad-slot="2860300775"
+      data-ad-format="auto"
+      data-full-width-responsive="true"></ins>
     // <div className="relative right-0 left-0  z-50 mx-auto h-48  max-w-lg  small:h-36 tall:h-36 long:h-48">
     //   <ins
     //     className="adsbygoogle"
